2f8928241b9f0a0ab8fc493044a92db99ffd084f
Augustina Eza <teenaeza@gmail.com>
2022-06-30 17:10:32 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
 /**
  * main - Programming is like building a multilingual puzzle
  * Return: 0 if exited properly
- */
- int main(void)
+*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
