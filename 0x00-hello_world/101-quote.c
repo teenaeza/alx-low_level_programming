ff588f952a3307ca79a3c0250d06ebc2f62fd366
Augustina Eza <teenaeza@gmail.com>
2022-07-01 02:11:03 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints to the standard error
+ * Return: 1 if it is executable
+ */
+int main(void)
+{
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
