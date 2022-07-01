a3e8a3fae8fa5824cde433a03fd09785ba699cb3
Augustina Eza <teenaeza@gmail.com>
2022-07-01 05:34:00 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",56,1,stderr);
 	return (1);
 }
