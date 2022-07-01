c0a7baee52f10b9edb385786409033404d45fa85
Augustina Eza <teenaeza@gmail.com>
2022-06-30 17:19:06 -0700

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* main - using the print function
+ * Return: 0 if exited properly
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
