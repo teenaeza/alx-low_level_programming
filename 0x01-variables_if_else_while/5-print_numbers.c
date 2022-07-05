e1cd907ad0246646035a72c16c0301bde6d10386
Augustina Eza <teenaeza@gmail.com>
2022-07-04 17:38:27 -0700

    alphabt

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print digits
+ * Return: 0 if it is executable
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <10; n++)
+	{
+		printf(%d, n);
+	}
+	printf("\n");
+	return (0);
+}
