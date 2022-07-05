26e49d20286dd794c4af0a6b6d76978be11391c7
Augustina Eza <teenaeza@gmail.com>
2022-07-04 17:08:58 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - write a program that prints the alphabets in lowercase
+ * Return: 0 if it is executable
+ */
+int main(void)
+{
+	int ch;
+	for (ch = 97; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10); /*this is an ascii code for new line*/
+	return(0);
+}
