98ce0f4093430c87b06d377061a5acdc6b2e4700
Augustina Eza <teenaeza@gmail.com>
2022-07-04 17:46:21 -0700

    alphabt

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program to calculate numbers
+ * Return: 0 if it is executable
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 48; ch <= 57; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10)
+
+		return (0);
+}
