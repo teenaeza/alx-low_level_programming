812e2b28afcbba8fe10a3c6e223fbb109c52ecc7
Augustina Eza <teenaeza@gmail.com>
2022-07-05 16:47:58 -0700

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+nclude "main.h"
+/**
+ * main - function to print abc 10 times
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	vhar c, i;
+	:wq
+		for (i = 0; i <= 9; i++)
+		{
+			for (c = 'a'; c <= 'z'; c++)
+			{
+				_putchar(c);
+			}
+			_putchar('\n');:
+		}
+}
