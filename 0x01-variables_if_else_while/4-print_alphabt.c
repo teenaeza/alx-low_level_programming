b104190255b9d25e45d1be360fc07515b8781f44
Augustina Eza <teenaeza@gmail.com>
2022-07-04 17:30:30 -0700

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints the lowercase alphabets exceept q and e
+ * Return: 0 if it is executable
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && != 'e')
+	{
+		putchar(ch);
+	}
+	}
+	putchar(10);
+
+	return (0);
+}
