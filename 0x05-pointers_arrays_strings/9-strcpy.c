1a5ba38e0a482f18a8d995ca1b783ff3fe269b11
Augustina Eza <teenaeza@gmail.com>
2022-07-13 16:25:32 -0700

    str

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a string
+ * @dest: a buffer
+ * @src: string to cpoy
+ * Return: a pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int index = 0;
+
+	for (index = 0; src[index] != '\0'; index++)
+	{
+		dest[index] = src[index];
+	}
+	dest[index] = '\0';
+
+	return (dest);
+}
