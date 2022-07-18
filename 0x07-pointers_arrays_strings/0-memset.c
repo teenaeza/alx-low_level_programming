c866c0c72adc472953a39481bd00ec1cdcc85036
Augustina Eza <teenaeza@gmail.com>
2022-07-18 12:21:28 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - fills
+ * @s: pointer
+ * @b: character
+ * @n: number
+ * Return: pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int index;
+	char *memory = s, value = b;
+
+	for(index = 0; index < n; index++)
+		memory[index] = value;
+
+	return (memory);
+}
