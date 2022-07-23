3b195db2aff52b7905a1a08bf7e3164aff59a145
Augustina Eza <teenaeza@gmail.com>
2022-07-23 16:35:46 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints
+ * @argc: no of args
+ * @argv: array
+ * Return: 1 or 0
+ */
+int main(int argc, char *argv[])
+{
+	int num, gigit, sum = 0;
+
+	for (num = 1; num < argc; num++)
+	{
+		for (digit = 0; argv[num][digit]; digit++)
+		{
+			if (argv[num][digit] < '0' || argv[num][digit] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[num]);
+	}
+
+	ptintf("%d\n", sum);
+
+	return (0);
+}
