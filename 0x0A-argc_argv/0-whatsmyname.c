bb5d3390c3e0516e951981aab8d1839a442e68c4
Augustina Eza <teenaeza@gmail.com>
2022-07-23 15:34:58 -0700

    name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints
+ * @argc: the no of arg
+ * @argv: array
+ * Return: always 0
+ */
+int main(int __attribute((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[]);
+
+	return (0);
+}
+
