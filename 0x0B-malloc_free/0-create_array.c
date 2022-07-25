bc8081bdeccd09f4714fb492726b966eabd94e31
Augustina Eza <teenaeza@gmail.com>
2022-07-25 05:37:48 -0700

    create

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	cr = malloc(sizeof(c) * size);
 
 	if (cr == NULL)
-			return  (NULL);
+		return  (NULL);
 
 	for (i = 0; i < size; i++)
 		cr[i] = c;
