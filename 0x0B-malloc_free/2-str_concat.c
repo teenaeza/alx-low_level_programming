4829287f2c98d0f82ba26037017e55bb59fceb5a
Augustina Eza <teenaeza@gmail.com>
2022-07-25 06:14:29 -0700

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,9 +12,9 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j, k, limit;
 
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
 	if (s2 == NULL)
-		s2 = " ";
+		s2 = "";
 
 	for (i = 0; s1[i] != '\0'; i++)
 		;
