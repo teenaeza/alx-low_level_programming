dbe83aa074d752fc4d6caa40b77499600ece1e32
Augustina Eza <teenaeza@gmail.com>
2022-07-29 05:49:58 -0700

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -183,11 +183,11 @@ int main(int argc, char *argv[])
 	if (*(argv[1]) == '0')
 		argv[1] = iterate_zeroes(argv[1]);
 	if (*(argv[2]) == '0')
-			argv[2] = iterate_zeroes(argv[2]);
+		argv[2] = iterate_zeroes(argv[2]);
 	if (*(argv[1]) == '\0' || *(argv[2]) == '\0')
 	{
-			printf("0\n");
-			return (0);
+		printf("0\n");
+		return (0);
 	}
 
 	size = find_len(argv[1]) + find_len(argv[2]);
