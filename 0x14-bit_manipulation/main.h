2365c8361077569949e609277e55e6355a06e5ea
Augustina Eza <teenaeza@gmail.com>
2022-08-18 06:10:25 -0700

    unit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,7 +6,7 @@
  * Desc: header file
  */
 int _putchar(char c);
-unsigned int binary_to_unit( const char *b);
+unsigned int binary_to_uint( const char *b);
 void print_binary(unsigned lon int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n,unsigned int index);
