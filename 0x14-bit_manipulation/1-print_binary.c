c4699661e441d89807f5aaf3c420da544b890864
Augustina Eza <teenaeza@gmail.com>
2022-08-18 06:34:07 -0700

    print

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_library - prints the binary rep. of a number
+ * print_binary - prints the binary rep. of a number
  * @n: the number to be printed in binary
  */
 void print_binary(unsigned long int n)
@@ -8,5 +8,5 @@ void print_binary(unsigned long int n)
 	if (n > 1)
 		print_binary(n >> 1);
 
-	_putchar(( n & 1) + '0');
+	_putchar((n & 1) + '0');
 }
