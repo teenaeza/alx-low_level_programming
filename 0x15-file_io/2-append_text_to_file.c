768f98e30aa0dde01a1877e2dd2e2e586c2dde07
Augustina Eza <teenaeza@gmail.com>
2022-08-24 03:09:42 -0700

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,13 +18,13 @@ int append_text_to_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	o = open(filename, O_WRONLY | 0_APPEND);
+	o = open(filename, O_WRONLY | O_APPEND);
 	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
 		return (-1);
 
-	closr(o);
+	close(o);
 
 	return (1);
 }
